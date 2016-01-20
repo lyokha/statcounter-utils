212fdc2b064b0ef4d25824c7b94d27e5b30bd4a9
lyokha <alexey.radkov@gmail.com>
2016-01-20 14:35:58 +0300

    further improvements in img_update.sh

--- img_update.sh
+++ img_update.sh
@@ -7,16 +7,16 @@ cities=$(sed -f cities_spells_fix.sed StatCounter-Log.csv | statcounter_report -
 
 for lines in 20 40 ; do
     let "height = $lines * $lheight"
-    echo "$cities" | head -$lines | tac > cities.csv
+    echo "$cities" | head -$lines > cities.csv
     gnuplot -e "datafile='cities.csv'; set term pngcairo size $width,$height; set lmargin 54; set output '${lines}cities.png'" stats.gpi
 done
 
 lines=$(echo "$cities" | wc -l)
 let "height = $lines * $lheight"
-echo "$cities" | tac > cities.csv
+echo "$cities" > cities.csv
 gnuplot -e "datafile='cities.csv'; set term pngcairo size $width,$height; set lmargin 54; set output 'cities.png'" stats.gpi
 
-statcounter_report -p -t'"^ "$8' StatCounter-Log.csv | awk -F\| '{print $2,";",$1}' | tac > countries.csv
+statcounter_report -p -t'"^ "$8' StatCounter-Log.csv | awk -F\| '{print $2,";",$1}' > countries.csv
 lines=$(wc -l countries.csv | cut -f1 -d' ')
 let "height = $lines * $lheight"
 gnuplot -e "datafile='countries.csv'; set term pngcairo size $width,$height; set lmargin 24; set output 'countries.png'" stats.gpi
